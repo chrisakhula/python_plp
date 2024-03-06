626142b5a1c43011ee31fd36682086db7df4a3dd
chrisakhula <ouma.cleo@gmail.com>
2024-03-06 17:57:08 +0300

    Apending by append & extend

--- slicing.py
+++ slicing.py
@@ -8,3 +8,25 @@ print(my_list[2:5])
 print(my_list[5:])
 
 # When we slice lists, the start index is inclusive but the end index is exclusive.
+
+
+# adding element to a python list
+
+numbers = [24, 14, 30, 54]
+
+print('Before append: ', numbers)
+
+# using append method
+numbers.append(32)
+
+print('After append: ', numbers)
+
+prime_numbers = [2, 3, 5]
+print('List1: ', prime_numbers)
+
+even_numbers = [4, 6, 8]
+print('List2: ', even_numbers)
+
+#join two lists
+prime_numbers.extend(even_numbers)
+print('List after append: ', prime_numbers)
