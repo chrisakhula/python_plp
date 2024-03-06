c9e9e2b2345983572f2775d2783517ea9136a7fb
chrisakhula <ouma.cleo@gmail.com>
2024-03-06 14:04:34 +0300

    Arithmetic Oeraters

---
+++ operaters.py
@@ -0,0 +1,8 @@
+# arithmentic operators
+print(5 + 6) # addition
+print(4 - 2) # subtraction
+print(2 * 3) # multiplication
+print(4 / 2) # division
+print(10 // 3) # floor division
+print(5 % 2) # modulo
+print(4 ** 2) # power
