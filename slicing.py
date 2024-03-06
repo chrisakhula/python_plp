5cb17c8292a47a55d2ee0aa1c8c85ffe1416ab18
chrisakhula <ouma.cleo@gmail.com>
2024-03-06 17:44:18 +0300

    slicing

---
+++ slicing.py
@@ -0,0 +1,10 @@
+# list slicing in python
+my_list = ['p','r','o','g','r','a','m','i']
+
+# items from index 2 to index 4
+print(my_list[2:5])
+
+# items from index 5 to end
+print(my_list[5:])
+
+# When we slice lists, the start index is inclusive but the end index is exclusive.
