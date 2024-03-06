1e3bd2ee70bc48c557b9e2d3772d24d972fdd9ab
chrisakhula <ouma.cleo@gmail.com>
2024-03-06 14:19:26 +0300

    Assignment operators

--- operaters.py
+++ operaters.py
@@ -6,3 +6,12 @@ print(4 / 2) # division
 print(10 // 3) # floor division
 print(5 % 2) # modulo
 print(4 ** 2) # power
+
+# assignement operaters
+a = 7 # assignment
+a += 1 # addition assigment
+a -= 3 # subtraction assignment
+a *= 4 # multiplication assignment
+a /= 3 # division assignment
+a %= 10 # remainder assignment
+a **= 10 # exponent assignment
